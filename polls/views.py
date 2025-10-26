38ba3f59322d6c65e45dcda2a7e843540256fabc
m0m3ntum123 <eliyevelay0@gmail.com>
2025-10-26 19:34:34 +0400

    Added Question model and ran migrations

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# filepath: c:\Users\Asus ROG\Downloads\.vscode\mydjangoapp\mysite\polls\views.py
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
