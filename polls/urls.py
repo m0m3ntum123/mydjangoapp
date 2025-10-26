8ed7b8b6d142f216092eee143cc212ce53356dc7
m0m3ntum123 <eliyevelay0@gmail.com>
2025-10-26 20:15:25 +0400

    Show choices for each question on detail page

--- polls/urls.py
+++ polls/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
-# filepath: c:\Users\Asus ROG\Downloads\.vscode\mydjangoapp\mysite\polls\views.py
-from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
