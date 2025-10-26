224265403c645273482a46c1a8aceeafa1159605
m0m3ntum123 <eliyevelay0@gmail.com>
2025-10-26 20:23:19 +0400

    Added voting functionality and results view

--- polls/urls.py
+++ polls/urls.py
@@ -12,3 +12,14 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/results/', views.results, name='results'),
+]
+
